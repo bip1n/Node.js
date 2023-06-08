51ad7a21bf330aa0f9d92ed7b7e751b979ed4b78
bipin <beepyn98166@gmail.com>
2023-06-08 22:29:10 +0545

    initial commit

---
+++ PathModule/path.js
@@ -0,0 +1,6 @@
+const path = require("path");
+// console.log(path.dirname('X:/Node.js/PathModule/path.js'))
+// console.log(path.extname('X:/Node.js/PathModule/path.js'))
+// console.log(path.basename('X:/Node.js/PathModule/path.js'))
+// console.log(path.parse('X:/Node.js/PathModule/path.js'))
+console.log(path.parse().name());
